66244bea9cd22c24ab79f4df32b39bb8f19cc4c2
Alexcravo <96731955+Alexcravo@users.noreply.github.com>
2022-04-08 17:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ TesteAPI/Entities/AgendaTarefas.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteAPI.Entities
+{
+    public class AgendaTarefas
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+        
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public DateTime HoraInicio { get; set; }
+
+        [Required]
+        public DateTime HoraFim { get; set; }
+
+        [Required]
+        public Prioridade Prioridade { get; set; }
+
+        [Required]
+        public string IsFinalizada { get; set; }
+    }
+
+    public enum Prioridade
+    {
+        Alta = 0,
+        Media = 1,
+        Baixa = 2
+    }
+}
